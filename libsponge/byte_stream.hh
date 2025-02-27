0898bc8e8cb1cfe81a3b819f118c98391dbfddf7
Andrew_Jung <jyjllll1025@postech.ac.kr>
2025-02-27 16:19:11 +0000

    all test passed

--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -1,8 +1,8 @@
 #ifndef SPONGE_LIBSPONGE_BYTE_STREAM_HH
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
+#include <deque>
 #include <string>
-
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
@@ -17,12 +17,11 @@ class ByteStream {
     // that's a sign that you probably want to keep exploring
     // different approaches.
     std::deque<char> buffer_;
-    size_t capacitiy_;
-    size_t bytes_writen_ = 0;
+    size_t capacity_;
+    size_t bytes_written_ = 0;
     size_t bytes_read_ = 0;
     bool end_ = false;
-
-    bool _error{} = false;  //!< Flag indicating that the stream suffered an error.
+    bool _error = false;
 
   public:
     //! Construct a stream with room for `capacity` bytes.
--- writeups/assn0.md
+++ writeups/assn0.md
@@ -7,11 +7,11 @@ My POVIS ID: jyjllll1025
 
 My student ID (numeric): 20210509
 
-This assignment took me about [n] hours to do (including the time on studying, designing, and writing the code).
+This assignment took me about 10 hours to do (including the time on studying, designing, and writing the code).
 
 My secret code from section 2.1 was: 7751ff3ee3
 
-- Optional: I had unexpected difficulty with: [describe]
+- Optional: I had unexpected difficulty with: It took me a while to get used to the work environment for the first time.
 
 - Optional: I think you could make this lab better by: [describe]
 
